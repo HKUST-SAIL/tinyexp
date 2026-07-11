c62e3cbf1ae06f341329cccd054dd8dac28953bc
Zane Li <zengarden2009@gmail.com>
2026-07-11 12:33:16 +0800

    refactor: ♻️ move config mixins into package

---
+++ tinyexp/exp_mixins/__init__.py
@@ -0,0 +1,8 @@
+from .mixins import CheckpointCfgMixin, RayCfgMixin, RedisCfgMixin, WandbCfgMixin
+
+__all__ = [
+    "CheckpointCfgMixin",
+    "RayCfgMixin",
+    "RedisCfgMixin",
+    "WandbCfgMixin",
+]
